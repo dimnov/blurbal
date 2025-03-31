2f3d250f1a9e9d1887bfd943ad621a7a4e49f528
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-03-31 22:57:28 +0300

    on mount checks the auth of a user

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,28 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { useAuth } from "@/hooks/useAuth";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const segments = useSegments();
+  const router = useRouter();
+  const session = useAuth();
+
+  useEffect(() => {
+    const isAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = session && session.user;
+
+    if (!isAuthScreen && !isSignedIn) router.replace("/(auth)");
+    else if (isAuthScreen && isSignedIn) router.replace("/(tabs)");
+  }, [session, session?.user]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
