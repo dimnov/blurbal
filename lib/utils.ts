cd4f7c77e0f3a966e2f616d4308c95e778ca7192
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-04-01 17:07:08 +0300

    Add date format helpers

---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+export function formatMemberSince(dateString: Date) {
+  const date = new Date(dateString);
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.getFullYear();
+
+  return `${month} ${year}`;
+}
+
+export function formatPublishDate(dateString: Date) {
+  const date = new Date(dateString);
+  const month = date.toLocaleString("default", { month: "long" });
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+}
