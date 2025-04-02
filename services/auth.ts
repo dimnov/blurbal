ce8af32d597a82d3909eca8c739948c63fde7717
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-04-03 02:29:01 +0300

    improve auth functions in services

--- services/auth.ts
+++ services/auth.ts
@@ -1,8 +1,7 @@
 import { supabase } from "@/lib/supabase";
-import { Alert } from "react-native";
 import { AVATAR_URL } from "@/constants/constants";
 
-export const signUp = async (username: string, email: string, password: string) => {
+export const signUpApi = async (username: string, email: string, password: string) => {
   try {
     const { data, error } = await supabase.auth.signUp({
       email,
@@ -14,61 +13,76 @@ export const signUp = async (username: string, email: string, password: string) 
       },
     });
 
-    if (error) {
-      Alert.alert(error.message);
-      return { data: null, error };
-    }
+    if (error) throw new Error(error.message);
 
     const userId = data.user?.id;
-    if (userId) await createUser(userId, username);
 
-    return { data, error: null }; // Ensure error is explicitly null when successful
-  } catch (error: any) {
-    Alert.alert(error.message);
-    return { data: null, error }; // Ensure return type consistency
+    if (userId) await createUser(userId, username, email);
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error("An unknown error occurred during sign up.");
+    }
   }
 };
 
-export const signIn = async (email: string, password: string) => {
+export const signInApi = async (email: string, password: string) => {
   try {
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
 
-    if (error) {
-      Alert.alert(error.message);
-      return { data: null, error };
+    if (error) throw new Error(error.message);
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error("An unknown error occurred during sign in.");
     }
-
-    return { data, error: null };
-  } catch (error: any) {
-    Alert.alert("Login Error", error.message);
-    return { data: null, error };
   }
 };
 
-export const createUser = async (userId: string, username: string) => {
+export const signOutApi = async () => {
+  try {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) throw new Error(error.message);
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error("An unknown error occurred during sign out.");
+    }
+  }
+};
+
+export const createUser = async (userId: string, username: string, email: string) => {
   try {
     const profileImage = `${AVATAR_URL}${username}`;
     const { error } = await supabase
       .from("profiles")
-      .insert({ id: userId, username, profileImage });
+      .insert({ id: userId, username, profileImage, email });
 
-    if (error) return Alert.alert(error.message);
+    if (error) throw new Error(error.message);
   } catch (err) {
-    Alert.alert("An unexpected error occurred.");
-    console.error("signOut error:", err);
+    if (err instanceof Error) {
+      throw new Error(err.message);
+    } else {
+      throw new Error("An unknown error occurred during sign in.");
+    }
   }
 };
 
-export const signOut = async () => {
+export const getUser = async (userId: string) => {
   try {
-    const { error } = await supabase.auth.signOut();
+    const { data, error } = await supabase.from("profiles").select("*").eq("id", userId).single();
 
-    if (error) return Alert.alert(error.message);
-  } catch (err) {
-    Alert.alert("An unexpected error occurred.");
-    console.error("signOut error:", err);
+    if (error) throw error;
+
+    return { data, error: null };
+  } catch (err: any) {
+    return { data: [], error: err.message };
   }
 };
