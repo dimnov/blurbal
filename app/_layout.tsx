feb14405c7a8afd635802c7c81b93d6977bed1b7
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-04-03 15:59:20 +0300

    Remove font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,25 @@
 import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { SplashScreen, Stack, useRouter, useSegments } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useFonts } from "expo-font";
 import SafeScreen from "@/components/SafeScreen";
 import { AuthProvider } from "@/context/AuthContext";
 import { useSession } from "@/hooks/auth/useSession";
-
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const segments = useSegments();
   const router = useRouter();
   const { session, isCheckingAuth } = useSession();
 
-  const [fontsLoaded] = useFonts({
-    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
-  });
-
-  useEffect(() => {
-    if (fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded]);
-
   useEffect(() => {
     const isAuthScreen = segments[0] === "(auth)";
-    const isSignedIn = session && session?.user;
+    const isSignedIn = session && session.user;
 
     if (!isAuthScreen && !isSignedIn) router.replace("/(auth)");
     else if (isAuthScreen && isSignedIn) router.replace("/(tabs)");
   }, [router, segments, session, session?.user]);
+
+  if (isCheckingAuth) return null;
 
   return (
     <AuthProvider>
