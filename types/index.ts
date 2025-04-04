9033758e2f68f2f0ff19f515a630666780989620
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-04-04 23:57:37 +0300

    Add type Book

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,22 @@ export type BookData = {
   title: string;
   caption: string;
   rating: number;
+  userId: string;
+};
+
+export type Book = {
+  title: string;
+  caption: string;
+  rating: number;
+  image: string;
+  imageBase64: string;
+};
+
+export type ProfileBoxProps = BookData & {
+  id: string;
+  userId: string;
+  created_at: Date;
+  image_url: string;
 };
 
 export type ItemData = BookData & {
@@ -19,4 +35,12 @@ export type RatingPickerProps = {
   rating: number;
   setRating: (rating: number) => void;
   n?: number;
+};
+
+export type Profile = {
+  id: string;
+  profileImage: string;
+  username: string;
+  email: string;
+  created_at: Date;
 };
